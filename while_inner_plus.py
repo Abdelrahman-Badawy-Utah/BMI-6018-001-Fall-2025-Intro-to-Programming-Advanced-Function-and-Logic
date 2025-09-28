a6d4a6910a39e2f4ddce53d2f400a1a31efd8ba1
Abdelrahman-Badawy-Utah <dr.abdel.rahman411@gmail.com>
2025-09-28 08:41:39 -0600

    while_inner_plus.py

--- while_inner_plus.py
+++ while_inner_plus.py
@@ -5,16 +5,17 @@ Created on Sun Sep 28 07:58:16 2025
 @author: El-Wattaneya
 """
 
-my_list = [1, 2, 3, 4, [5, 6, 7, [8, 9]]]
+input_list = [1, 2, 3, 4, [5, 6, 7, [8, 9]]]
 
 while True:
 #first, we check if the last element is a list     
-    if isinstance(my_list[-1], list):
-        my_list = my_list[-1]  # and if the last element is a list, it should assign my_list to the innermost list
+    if isinstance(input_list[-1], list):
+        input_list = input_list[-1]  # and if the last element is a list, it should assign my_list to the innermost list
     else:
         break  # it will break the loop if there are no more innermost lists
 
-# now we add 1 to each element inside the innermost list (my_list) 
-result = [x + 1 for x in my_list]
+# now we add 1 to each element inside the innermost list (input_list) 
+result = [x + 1 for x in input_list]
+
 
 print(result)  
